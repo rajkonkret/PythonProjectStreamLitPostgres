b3055b774f61fa38dda744ae034230fd4aff4140
radek <rajkonkret660@gmail.com>
2026-09-23 12:06:09 +0200

    [1 17] update zad4.py

--- zad5_slownik.py
+++ zad5_slownik.py
@@ -1,7 +1,20 @@
 import os
 import psycopg
 from dotenv import load_dotenv
+from psycopg.rows import dict_row
 
 load_dotenv()
 
 DATABASE_URL = os.environ["DATABASE_URL"]
+
+with psycopg.connect(DATABASE_URL, row_factory=dict_row) as conn:
+    with conn.cursor() as curr:
+        curr.execute("""
+        SELECT id, name, price FROM products
+        """)
+
+        product = curr.fetchone()
+
+        print(product)
+        print(product['name'])
+        # Ergonomiczny Mysz v3
