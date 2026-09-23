67d4d74ed0347bda379b0237429565299eeae037
radek <rajkonkret660@gmail.com>
2026-09-23 12:19:15 +0200

    [1 19] update zad6_insert.py

--- zad6_insert.py
+++ zad6_insert.py
@@ -7,6 +7,53 @@ load_dotenv()
 
 DATABASE_URL = os.environ["DATABASE_URL"]
 
-conn = psycopg.connect[DATABASE_URL]
+conn = psycopg.connect(DATABASE_URL)
 
 cur = conn.cursor()
+
+# CRUD
+
+cur.execute(
+    """
+    INSERT INTO products (name, price, quantity)
+    VALUES (%s, %s, %s)
+    """,
+    ("Laptop", 3999.99, 5)
+)
+
+conn.commit()
+
+# RETURNING
+cur.execute(
+    """
+    INSERT INTO products (name, price, quantity)
+    VALUES (%s, %s, %s)
+    RETURNING id, name, price, quantity
+    """,
+    ("Laptop", 3999.99, 5)
+)
+product = cur.fetchone()
+
+print(product)
+# (108, 'Laptop', Decimal('3999.99'), 5)
+
+name = "Laptop"
+
+# cur.execute(
+#     f"SELECT * FROM products WHERE name='{name}'"
+# )
+
+cur.execute(
+    """
+    SELECT *
+    FROM products
+    WHERE name = %s
+    """,
+    (name,)
+)
+product = cur.fetchone()
+
+print(product)
+
+conn.commit()
+conn.close()
