3c677f3b12ec30de9e1844f897d9efdf8240e12f
radek <rajkonkret660@gmail.com>
2026-09-23 11:17:07 +0200

    [1 5] update zad1.py

--- zad1.py
+++ zad1.py
@@ -11,3 +11,13 @@ import psycopg
 # pip install "psycopg[binary]"
 
 print(psycopg.__version__)  # 3.3.6
+
+conn = psycopg.connect(
+    dbname="shop",
+    user="postgres",
+    password="sekret",
+    host="localhost",
+    port=5433
+)
+
+
