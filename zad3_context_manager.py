d633f20272b1c928466cadf7a64ed232cbf522c1
radek <rajkonkret660@gmail.com>
2026-09-23 11:46:11 +0200

    [1 12] update zad3.py

---
+++ zad3_context_manager.py
@@ -0,0 +1,13 @@
+import os
+import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+conn = psycopg.connect(DATABASE_URL)
+
+with conn.cursor() as cur:
+    cur.execute("SELECT version()")
+    print(cur.fetchone())
