d37f336836d4040662aa94e143d07573d4efe7c1
radek <rajkonkret660@gmail.com>
2026-09-23 11:03:16 +0200

    [1 4] update zad1.py

--- zad1.py
+++ zad1.py
@@ -4,3 +4,10 @@
 # asyncpg - aplikacje asynchroniczne
 # orm - mapowanie opiektowo relacyjna
 # sqlalchemy
+
+import psycopg
+
+# ctrl / - komentarz
+# pip install "psycopg[binary]"
+
+print(psycopg.__version__)  # 3.3.6
